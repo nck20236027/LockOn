7e8dcac66e4cce6fcfd2f902ceac58064c2f6f7b
nkc_horiki <nkc23.horiki.kousei@gmail.com>
2025-02-17 16:37:19 +0900

    Add:燃料ゲージの完成　ただしクソ汚いしモデル側の書き方が特殊になる　改善前提の機能

--- Assets/Project/20236027/Script/TargetUIFolder/TargetUI.cs
+++ Assets/Project/20236027/Script/TargetUIFolder/TargetUI.cs
@@ -20,6 +20,7 @@ public class TargetUI : MonoBehaviour, IhasTargetPos
     // Update is called once per frame
     void Update()
     {
+
         if(Input.GetMouseButtonDown(0))
         {
             UIMediator.Instance.Hide(param);
