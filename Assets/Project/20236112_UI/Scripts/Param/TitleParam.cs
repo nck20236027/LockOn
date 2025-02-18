48f6bd8e6e1c9b4f304c476399167281113ae7b3
nkc20236112 <nkc23.serigano.masato@gmail.com>
2025-02-18 16:41:40 +0900

    Add:TitleシーンにUIの見た目だげ配置

---
+++ Assets/Project/20236112_UI/Scripts/Param/TitleParam.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleParam :ParamBase
+{
+    public int currentIndex;
+ 
+}
---
+++ Assets/Project/20236112_UI/Scripts/View/TitleView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleView : ViewBase
+{
+    [SerializeField]
+    private Text[] texts;
+    [SerializeField]
+    private int _currentIndex;
+
+}
