f77878a8fb2bebe983db6c53800b8e7161b63fad
nkc_horiki <nkc23.horiki.kousei@gmail.com>
2025-02-28 18:37:03 +0900

    update:ゲームクリアシーン作成

--- Assets/Project/20236027/Script/GameOverMovieFolder/GameOverMovieManager.cs
+++ Assets/Project/20236027/Script/GameOverMovieFolder/GameOverMovieManager.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class GameOverMovieManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject cameraObject;
+    [SerializeField] private ParticleSystem ParticleObject;
+    [SerializeField] private float startExplosion;
+    [SerializeField] private float endExplosion;
+
+    async void Start()
     {
-        
+        await UniTask.WaitForSeconds(startExplosion);
+        ParticleObject.Play();
+        await UniTask.WaitForSeconds(endExplosion);
+        Destroy(cameraObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
