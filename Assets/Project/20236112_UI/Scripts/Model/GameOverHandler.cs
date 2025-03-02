dbcd5ccbfdf333a4ec83065a340b8b81fc36eb94
DESKTOP-91B2EOJ\iankn <ianknot@icloud.com>
2025-03-02 18:35:08 +0900

    Fix:デバッグできるようにエラーをコメント化

--- Assets/Project/20236112_UI/Scripts/Model/GameOverHandler.cs
+++ Assets/Project/20236112_UI/Scripts/Model/GameOverHandler.cs
@@ -4,23 +4,33 @@ using System.Collections.Generic;
 
 public class GameOverHandler : MonoBehaviour
 {
-    private TitleParam titleParam;
-    private GameOverParam _gameOverParam = new();
-    private GameOverInputHandler _gameOverInputHandler;
-
     private List<IGameOverAction> _gameOverAction = new();
-    //private List<IQuitAction> _quitAction = new();
+
+    //private TitleParam titleParam;
+    private GameOverParam _gameOverParam = new();
+
+    private GameOverInputHandler _gameOverInputHandler = new();
+
+    private QuitParam quitParam = new();
+    private GameQuiter gameQuiter = new GameQuiter();
 
     private int _currentIndex;
 
     private void Awake()
     {
-        _gameOverAction.Add(new GameRestert(_gameOverParam, _gameOverInputHandler.));
+        _gameOverAction.Add(new GameRestert(_gameOverParam, _gameOverInputHandler.SetReStartInput));
+        _gameOverAction.Add(new GameQuiter());
+
+        //_gameOverParam.currentIndex = 0;
+
+        _gameOverInputHandler._onGameOverSubmit += OnGameEndSubmit;
+        //_gameOverInputHandler._onGameOverChoice += ;
+        _gameOverInputHandler.Init();
+
     }
 
     private void Start()
     {
-        _gameOverParam.currentIndex = 0;
         ServiceLocator<UIMediator>.GetInstance().Init(_gameOverParam);
     }
 
@@ -32,15 +42,35 @@ public class GameOverHandler : MonoBehaviour
         }
     }
 
-    public void OnGameOverAction()
-    {
-
-    }
-
-
     public void OnGameEndSubmit()
     {
         _gameOverAction[_currentIndex].OnGameOverAction();
+    }
+
+    public void QuitChoice(float direction)
+    {
+        ////Debug.Log(_quitCurrentIndex);
+        //_quitCurrentIndex -= (int)direction;
+        //if (_quitCurrentIndex > 1)
+        //{
+        //    _quitCurrentIndex = 1;
+        //    return;
+        //}
+        //if (_quitCurrentIndex < 0)
+        //{
+        //    _quitCurrentIndex = 0;
+        //    return;
+        //}
+        //_gameOverParam.currentIndex = _quitCurrentIndex;
+        //ServiceLocator<UIMediator>.GetInstance().Reload();
+
+    }
+
+    private void OnDestroy()
+    {
+        _gameOverInputHandler.Final();
+        //ServiceLocator<UIMediator>.GetInstance().Final(_gameEndParam);
+        Debug.Log("onDestroy");
     }
 
 }
